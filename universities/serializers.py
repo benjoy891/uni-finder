6f6a6f322b5b736d69eda7b3c56ea6d69ed9fd6a
Ben Joy <benjoybj891@gmail.com>
2026-08-10 13:15:18 +0530

    feat: Completed University CRUD operations.

--- universities/serializers.py
+++ universities/serializers.py
@@ -1,8 +1,37 @@
 from rest_framework import serializers
-from .models import University
+from .models import University, AdmissionRequirement, LanguageRequirement, Program
 
 class UniversitySerializer(serializers.ModelSerializer):
     class Meta:
         model = University
-        fields = ["id", "name", "country", "city", "address", "website"]
+        fields = ["id", "name", "country", "city", "address", "website", "created_at", "updated_at"]
 
+
+class AdmissionRequirementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdmissionRequirement
+        fields = ["minimum_gpa", "minimum_german_gpa", "gre", "gmat", "aps_required", "notes"]
+
+
+class LanuageRequirementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LanguageRequirement
+        fields = ["language", "exam", "minimum_score"]
+
+class ProgramSerializer(serializers.ModelSerializer):
+    admission_requirement = AdmissionRequirementSerializer(read_only=True)
+    language_requirements = LanuageRequirementSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Program
+        fields = ["id", "name", "degree", "teaching_language", "semester", "application_start", "application_end",
+                  "application_platform", "vpd_required", "nc_status", "program_url", "description", "admission_requirement",
+                  "language_requirements"]
+
+
+
+class UniversityDetailSerializer(serializers.ModelSerializer):
+    programs = ProgramSerializer(many=True, read_only=True)
+    class Meta:
+        model = University
+        fields = ["id", "name", "country", "city", "address", "website", "programs", "created_at", "updated_at"]
--- universities/views.py
+++ universities/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status, serializers
 
 from .models import University
-from .serializers import UniversitySerializer
+from .serializers import UniversityDetailSerializer, UniversitySerializer
 import logging
 logger = logging.getLogger(__name__)
 
@@ -23,8 +23,7 @@ class UniversityListView(APIView):
                 "result" : True, 
                 "message": "Universities retrieved successfully.",
                 "data": serializer.data
-            }, status=status.HTTP_200_OK)
-        
+            }, status=status.HTTP_200_OK)        
         except Exception as e:
             logger.exception("Unexpected error while retrieving universities: %s",e)
             return Response({
@@ -35,6 +34,30 @@ class UniversityListView(APIView):
                     "message": "Something went wrong. Please try again later."
                 },status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    def post(self, request):
+        try:
+            serializer = UniversitySerializer(data=request.data)
+            if serializer.is_valid():
+                university = serializer.save()
+                return Response({
+                    "result": True,
+                    "message": "University created successfully.",
+                    "data": UniversitySerializer(university).data
+                }, status=status.HTTP_201_CREATED)
+
+            return Response({
+                "result": False,
+                "message": "Invalid University data.",
+                "data": serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logger.exception("Unexpected error while creating university: %s", e)
+            return Response({
+                "result": False,
+                "message" : "An unexpected error occured",
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        
 
 class UniversityDetailView(APIView):
     def get(self, request, pk):
@@ -47,8 +70,7 @@ class UniversityDetailView(APIView):
                     "type": "Not Found",
                 },
                 "message": "University Not Found."
-            },status=status.HTTP_404_NOT_FOUND)
-        
+            },status=status.HTTP_404_NOT_FOUND)        
         except Exception as e:
             logger.exception("Unexpected error while retrieving university %s: %s", pk, e)
             return Response({
@@ -57,12 +79,64 @@ class UniversityDetailView(APIView):
                         "type": "Internal Server Error"
                     },
                     "message": "Something went wrong. Please try again later."
-                },status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        serializer = UniversitySerializer(university)
-
+                },status=status.HTTP_500_INTERNAL_SERVER_ERROR)        
+        serializer = UniversityDetailSerializer(university)
         return Response({
             "result": True,
             "message": "University retrieved successfully",
             "data": serializer.data
         }, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        try:
+            university = University.objects.get(pk=pk)
+        except University.DoesNotExist:
+            return Response({
+                "result": False,
+                "message": "University Not Found."
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as e:
+            logger.exception("Unexpected error while retrieving university %s: %s", pk, e)
+            return Response({
+                "result": False,
+                "message": "Something went wrong."
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        serializer = UniversitySerializer(
+            university, 
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            university = serializer.save()
+            return Response({
+                "result": True,
+                "message": "University updated successfully.",
+                "data": UniversityDetailSerializer(university).data
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "result": False,
+            "message": "Invalid University data.",
+            "data": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        try:
+            university = University.objects.get(pk=pk)
+            university.delete()
+            return Response({
+                "result": True,
+                "message": "University deleted successfully."
+            }, status=status.HTTP_204_NO_CONTENT)
+        except University.DoesNotExist:
+            return Response({
+                "result": False,
+                "message": "University Not Found."
+            }, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            logger.exception("Unexpected error while deleting university %s: %s", pk, e)
+            return Response({
+                "result": False,
+                "message": "Something went wrong."
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
